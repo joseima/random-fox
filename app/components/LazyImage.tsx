78a8c711e8bf5b1b7db3e664d4670f5f15b76e2c
joseima <joseima17@gmail.com>
2024-03-04 15:55:39 +0100

    declared generic types

---
+++ app/components/LazyImage.tsx
@@ -0,0 +1,49 @@
+import { useRef, useEffect, useState, use } from "react";
+import type { ImgHTMLAttributes } from "react";
+
+// import type { FunctionComponent, FC } from "react"
+type LazyImageProps = {src: string};
+type ImageNative = ImgHTMLAttributes<HTMLImageElement>; // add native properties of an iamge element for future usabuilitie, Not required for lazy loading
+type Props = LazyImageProps & ImageNative;
+
+
+export const LazyImage = ({src, ...imgProps}: Props): JSX.Element => {
+    const node = useRef<HTMLImageElement>(null); //add element and inicialize with null
+    const[currentSrc, setCurrentSrc] = useState("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=");
+   
+    useEffect( () => {
+        //new observer
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (!entry.isIntersecting || !node.current) {
+                  return;
+                }
+        
+                setCurrentSrc(src);
+              });
+        });
+        // observe node
+        if (node.current) {
+            observer.observe(node.current);
+        }
+
+        //disconnect
+        return () => {
+            observer.disconnect() 
+        }
+    }, [src]);
+    return <img 
+        ref={node} 
+        src={currentSrc} 
+        {...imgProps} //additionl funcionalities
+     /> 
+} // most used
+
+
+// export const RandomFox2: FunctionComponent = () => {
+//     return <img /> 
+// }
+
+// export const RandomFox3: FC = () => {
+//     return <img /> 
+// }
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,41 @@
+'use client';
+import { useState } from "react";
+import type { MouseEventHandler  } from "react";
 import { NextPage } from "next";
-import { RandomFox } from "./components/RandomFox";
+import { LazyImage } from "./components/LazyImage";
 
 const random = () => Math.floor(Math.random() * 123) + 1;
+const generateId = () => Math.random().toString(36).substr(2,9);
 
 const Home: NextPage = () => {
+
+const [images, setImages] = useState<Array<IImageItem>>([]);
+
+const addNewFox : MouseEventHandler<HTMLButtonElement> = (event) => {
+  event.preventDefault();
+  const newImageItem :IImageItem = {
+    id:generateId(), url: `https://randomfox.ca/images/${random()}.jpg`
+  };
+  setImages([
+    ...images,
+    newImageItem
+  ])
+}
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl font-bold mb-6">Random Fox</h1>
-      <RandomFox image={`https://randomfox.ca/images/${random()}.jpg`} />
+    <main className="flex min-h-screen flex-col items-center  p-24">
+      <h1 className="text-3xl font-bold mb-6">Random Images with Lazy Loading</h1>
+      <button onClick={addNewFox} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Add new fox</button>
+      { images.map(({id, url}) => (
+        <div key={id} className="p-4">
+          <LazyImage
+               src={url}
+                width={320} 
+                height="auto" 
+                className="mx-auto bg-gray-300 rounded-lg"
+          />
+        </div>
+        ))}
     </main>
   );
 }
